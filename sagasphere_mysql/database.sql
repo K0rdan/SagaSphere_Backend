c90a5c864175122dd8246a5387bdfd3ad904809f
Benjamin <benjamin.christau@oyez.fr>
2017-05-22 00:20:49 +0200

    First SQL implementation

---
+++ sagasphere_mysql/database.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : sagasphere_mysql
+-- Généré le :  Dim 21 mai 2017 à 22:17
+-- Version du serveur :  5.7.18
+-- Version de PHP :  7.0.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `sagasphere`
+--
+CREATE DATABASE IF NOT EXISTS `sagasphere` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `sagasphere`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sagas`
+--
+
+CREATE TABLE `sagas` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `creation` date NOT NULL,
+  `url` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tracks`
+--
+
+CREATE TABLE `tracks` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `duration` int(11) NOT NULL,
+  `creation` date NOT NULL,
+  `sagaID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `sagas`
+--
+ALTER TABLE `sagas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `tracks`
+--
+ALTER TABLE `tracks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sagaID` (`sagaID`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `sagas`
+--
+ALTER TABLE `sagas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `tracks`
+--
+ALTER TABLE `tracks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `tracks`
+--
+ALTER TABLE `tracks`
+  ADD CONSTRAINT `tracks_fk_sagas` FOREIGN KEY (`sagaID`) REFERENCES `sagas` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
