839e4fcdda4f62c51938282d85894bcae60673db
Benjamin <benjamin.christau@oyez.fr>
2017-05-29 18:28:16 +0200

    Update database

--- sagasphere_mysql/database-dev.sql
+++ sagasphere_mysql/database-dev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : sagasphere_mysql
--- Généré le :  lun. 22 mai 2017 à 13:17
+-- Généré le :  lun. 29 mai 2017 à 16:26
 -- Version du serveur :  5.7.18
 -- Version de PHP :  7.0.16
 
@@ -25,61 +25,67 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `sagaID` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `url` text NOT NULL,
+  `title` text NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `news`
+--
+
+INSERT INTO `news` (`id`, `sagaID`, `date`, `url`, `title`, `content`) VALUES
+(1, 1, '2017-04-11 00:00:00', 'http://www.penofchaos.com/donjon/', 'Gurdil Métal', 'Cette semaine, grâce au travail de toute l\'équipe de Magoyond et du Naheulband, nous sommes en mesure de vous offrir une belle version métal de Mon Ancêtre Gurdil, enregistrée au studio Magoyond. Tous les liens pour écouter ou télécharger le fichier sont sur notre site.');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `sagas`
 --
+
 DROP TABLE IF EXISTS `sagas`;
 CREATE TABLE `sagas` (
   `id` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `image` varchar(200) NOT NULL,
-  `author` varchar(50) NOT NULL,
+  `title` tinytext NOT NULL,
+  `image` text NOT NULL,
+  `author` tinytext NOT NULL,
   `creation` date NOT NULL,
-  `url` varchar(200) NOT NULL
+  `url` text NOT NULL,
+  `newsUrl` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `sagas`
 --
 
-INSERT INTO `sagas` (`id`, `title`, `image`, `author`, `creation`, `url`) VALUES
-(1, 'Donjon de Naheulbeuk', 'http://streaming.nekonosekai.com/images/LOGO_Naheulbeuk.png', 'Pen of Chaos', '2001-01-01', 'http://www.penofchaos.com/warham/donjon.htm');
+INSERT INTO `sagas` (`id`, `title`, `image`, `author`, `creation`, `url`, `newsUrl`) VALUES
+(1, 'Donjon de Naheulbeuk', 'http://streaming.nekonosekai.com/images/LOGO_Naheulbeuk.png', 'Pen of Chaos', '2001-01-01', 'http://www.penofchaos.com/warham/donjon.htm', 'http://www.penofchaos.com/naheulbeukrss.xml'),
+(2, 'Reflet d\'Acide', 'http://streaming.nekonosekai.com/images/LOGO_RDA.png', 'JBX', '2011-01-01', 'http://www.refletsdacide.com/', 'http://www.refletsdacide.com/feed/');
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `tracks`
 --
+
 DROP TABLE IF EXISTS `tracks`;
 CREATE TABLE `tracks` (
   `id` int(11) NOT NULL,
   `trackNumber` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `name` tinytext NOT NULL,
   `duration` int(11) NOT NULL,
-  `url` varchar(200) NOT NULL,
+  `url` text NOT NULL,
   `creation` date NOT NULL,
   `sagaID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(24) NOT NULL COMMENT '',
-  `pass` varchar(16) NOT NULL COMMENT '',
-  `email` varchar(254) NOT NULL COMMENT 'RFC compliance'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `pass`, `email`) VALUES
-(1, 'test', 'pass', 'test@test.fr'),
-(2, 'Kordan', 'pass', 'test@test.fr');
 
 --
 -- Déchargement des données de la table `tracks`
@@ -88,9 +94,59 @@ INSERT INTO `users` (`id`, `name`, `pass`, `email`) VALUES
 INSERT INTO `tracks` (`id`, `trackNumber`, `name`, `duration`, `url`, `creation`, `sagaID`) VALUES
 (1, 1, 'Épisode 01 : Présentation ', 255, 'http://naheulbeuk.tamdb.net/donjon01.zip', '2001-01-01', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` tinytext NOT NULL,
+  `pass` tinytext NOT NULL,
+  `email` varchar(254) NOT NULL COMMENT 'RFC compliance'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `pass`, `email`) VALUES
+(1, 'test', 'pass', 'test@test.fr'),
+(2, 'Kordan', 'pass', 'test@test.fr');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_feeds`
+--
+
+DROP TABLE IF EXISTS `user_feeds`;
+CREATE TABLE `user_feeds` (
+  `id` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `sagaID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user_feeds`
+--
+
+INSERT INTO `user_feeds` (`id`, `userID`, `sagaID`) VALUES
+(1, 1, 1),
+(2, 1, 2);
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sagaID` (`sagaID`);
 
 --
 -- Index pour la table `sagas`
@@ -112,14 +168,27 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `user_feeds`
+--
+ALTER TABLE `user_feeds`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `sagaID` (`sagaID`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
+-- AUTO_INCREMENT pour la table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT pour la table `sagas`
 --
 ALTER TABLE `sagas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `tracks`
 --
@@ -131,14 +200,32 @@ ALTER TABLE `tracks`
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT pour la table `user_feeds`
+--
+ALTER TABLE `user_feeds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `news`
+--
+ALTER TABLE `news`
+  ADD CONSTRAINT `news_ibfk_1` FOREIGN KEY (`sagaID`) REFERENCES `sagas` (`id`);
 
 --
 -- Contraintes pour la table `tracks`
 --
 ALTER TABLE `tracks`
   ADD CONSTRAINT `tracks_fk_sagas` FOREIGN KEY (`sagaID`) REFERENCES `sagas` (`id`);
+
+--
+-- Contraintes pour la table `user_feeds`
+--
+ALTER TABLE `user_feeds`
+  ADD CONSTRAINT `user_feeds_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `user_feeds_ibfk_2` FOREIGN KEY (`sagaID`) REFERENCES `sagas` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- sagasphere_mysql/database-prod.sql
+++ sagasphere_mysql/database-prod.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : sagasphere_mysql
--- Généré le :  Dim 21 mai 2017 à 22:17
+-- Généré le :  lun. 29 mai 2017 à 16:26
 -- Version du serveur :  5.7.18
 -- Version de PHP :  7.0.16
 
@@ -21,22 +21,38 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `sagasphere`
 --
-CREATE DATABASE IF NOT EXISTS `sagasphere` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `sagasphere`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `sagaID` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `url` text NOT NULL,
+  `title` text NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `sagas`
 --
+
 DROP TABLE IF EXISTS `sagas`;
 CREATE TABLE `sagas` (
   `id` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `image` varchar(200) NOT NULL,
-  `author` varchar(50) NOT NULL,
+  `title` tinytext NOT NULL,
+  `image` text NOT NULL,
+  `author` tinytext NOT NULL,
   `creation` date NOT NULL,
-  `url` varchar(200) NOT NULL
+  `url` text NOT NULL,
+  `newsUrl` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -44,11 +60,14 @@ CREATE TABLE `sagas` (
 --
 -- Structure de la table `tracks`
 --
+
 DROP TABLE IF EXISTS `tracks`;
 CREATE TABLE `tracks` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `trackNumber` int(11) NOT NULL,
+  `name` tinytext NOT NULL,
   `duration` int(11) NOT NULL,
+  `url` text NOT NULL,
   `creation` date NOT NULL,
   `sagaID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -58,17 +77,38 @@ CREATE TABLE `tracks` (
 --
 -- Structure de la table `users`
 --
+
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL COMMENT '',
-  `pass` varchar(50) NOT NULL COMMENT '',
+  `name` tinytext NOT NULL,
+  `pass` tinytext NOT NULL,
   `email` varchar(254) NOT NULL COMMENT 'RFC compliance'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_feeds`
+--
+
+DROP TABLE IF EXISTS `user_feeds`;
+CREATE TABLE `user_feeds` (
+  `id` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `sagaID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sagaID` (`sagaID`);
 
 --
 -- Index pour la table `sagas`
@@ -90,35 +130,64 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `user_feeds`
+--
+ALTER TABLE `user_feeds`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userID` (`userID`),
+  ADD KEY `sagaID` (`sagaID`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
+-- AUTO_INCREMENT pour la table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT pour la table `sagas`
 --
 ALTER TABLE `sagas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `tracks`
 --
 ALTER TABLE `tracks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `user_feeds`
+--
+ALTER TABLE `user_feeds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `news`
+--
+ALTER TABLE `news`
+  ADD CONSTRAINT `news_ibfk_1` FOREIGN KEY (`sagaID`) REFERENCES `sagas` (`id`);
 
 --
 -- Contraintes pour la table `tracks`
 --
 ALTER TABLE `tracks`
   ADD CONSTRAINT `tracks_fk_sagas` FOREIGN KEY (`sagaID`) REFERENCES `sagas` (`id`);
+
+--
+-- Contraintes pour la table `user_feeds`
+--
+ALTER TABLE `user_feeds`
+  ADD CONSTRAINT `user_feeds_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `user_feeds_ibfk_2` FOREIGN KEY (`sagaID`) REFERENCES `sagas` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
