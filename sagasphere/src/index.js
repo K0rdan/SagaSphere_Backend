de0d637f0ccce13edf5e3fbf095839c34a5b4e65
Benjamin Christau <christau_benjamin@hotmail.com>
2017-09-07 10:44:11 +0200

    [New] Implement retries and timeout on server intialization.

--- sagasphere/src/index.js
+++ sagasphere/src/index.js
@@ -34,81 +34,112 @@ if (process.env.DEBUG === "true") {
 }
 
 function initServer() {
-    return new Promise((resolve) => {
-        app.disable("x-powered-by");
-        app.use((req, res, next) => {
-            res.header("Access-Control-Allow-Origin", "*"); // Allow Origin : All
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            res.header("Access-Control-Allow-Methods", "GET,POST");
-            res.header("Access-Control-Allow-Credentials", "true");
-            next();
-        });
-        app.use(cookieParser()); // Allow cookies
-        app.use(bodyParser.json()); // Allow body type : JSON
-        app.use(bodyParser.urlencoded({ extended: true })); // Allow body type : URL-encoded
-        // Check if sessions are activated
-        if (process.env.SAGASPHERE_REDIS_SECRET) {
-            app.use(session({
-                secret: process.env.SAGASPHERE_REDIS_SECRET,
-                saveUninitialized: false, // No session for unauthorized users
-                resave: false, // Disable unmodified session saving
-                cookie: {
-                    domain: process.env.SAGASPHERE_COOKIE_DOMAIN || "localhost",
-                    path: process.env.SAGASPHERE_COOKIE_PATH || "/",
-                    httpOnly: true,
-                    maxAge: process.env.SAGASPHERE_COOKIE_MAXAGE || 24 * 60 * 60 * 1000 // Default : 24h
-                }
-            }));
-        }
-        app.use((req, res, next) => {
-            res.promise = (promise) => {
-                promise
-                    .then((resObj) => {
-                        res.status(resObj.code).json({ status: "ok", message: resObj.message, data: resObj.data });
-                    })
-                    .catch((err) => {
-                        // Add specific route tag to the list
-                        logTags.push(err.route);
-                        if (err) {
-                            if (err.code >= 400 && err.code < 500) {
-                                Log.warn(logTags, "User error", err.error);
-                            }
-                            else if (err.code >= 500) {
-                                Log.err(logTags, "Internal error", err.error);
+    let retries = Config.connexion.MySQL.retries;
+    let intervalID = null;
+
+    return new Promise((resolve, reject) => {
+        intervalID = setInterval(() => {
+            app.disable("x-powered-by");
+            app.use((req, res, next) => {
+                res.header("Access-Control-Allow-Origin", "*"); // Allow Origin : All
+                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+                res.header("Access-Control-Allow-Methods", "GET,POST");
+                res.header("Access-Control-Allow-Credentials", "true");
+                next();
+            });
+            app.use(cookieParser()); // Allow cookies
+            app.use(bodyParser.json()); // Allow body type : JSON
+            app.use(bodyParser.urlencoded({ extended: true })); // Allow body type : URL-encoded
+            // Check if sessions are activated
+            if (process.env.SAGASPHERE_REDIS_SECRET) {
+                app.use(session({
+                    secret: process.env.SAGASPHERE_REDIS_SECRET,
+                    saveUninitialized: false, // No session for unauthorized users
+                    resave: false, // Disable unmodified session saving
+                    cookie: {
+                        domain: process.env.SAGASPHERE_COOKIE_DOMAIN || "localhost",
+                        path: process.env.SAGASPHERE_COOKIE_PATH || "/",
+                        httpOnly: true,
+                        maxAge: process.env.SAGASPHERE_COOKIE_MAXAGE || 24 * 60 * 60 * 1000 // Default : 24h
+                    }
+                }));
+            }
+            app.use((req, res, next) => {
+                res.promise = (promise) => {
+                    promise
+                        .then((resObj) => {
+                            res.status(resObj.code).json({ status: "ok", message: resObj.message, data: resObj.data });
+                        })
+                        .catch((err) => {
+                            // Add specific route tag to the list
+                            logTags.push(err.route);
+                            if (err) {
+                                if (err.code >= 400 && err.code < 500) {
+                                    Log.warn(logTags, "User error", err.error);
+                                }
+                                else if (err.code >= 500) {
+                                    Log.err(logTags, "Internal error", err.error);
+                                }
+                                else {
+                                    Log.err(logTags, "Unhandled error", err.error);
+                                }
+                                res.status(err.code).json({ status: "ko", message: err.message });
                             }
                             else {
-                                Log.err(logTags, "Unhandled error", err.error);
+                                Log.err(logTags, "Unhandled error");
                             }
-                            res.status(err.code).json({ status: "ko", message: err.message });
-                        }
-                        else {
-                            Log.err(logTags, "Unhandled error");
-                        }
-                        // Remove the route tag
-                        logTags.pop();
-                    });
-            };
-            next();
-        });
-        app.listen(port, () => {
-            Log.info(logTags, `Server listening on port ${port}.`);
-            resolve();
-        });
+                            // Remove the route tag
+                            logTags.pop();
+                        });
+                };
+                next();
+            });
+            app.listen(port, process.env.IPADRESS, (err) => {
+                if (err) {
+                    if (retries > 0) {
+                        retries--;
+                    }
+                    else {
+                        clearInterval(intervalID);
+                        Log.err(logTags, `Server initialisation failed for the ${Config.connexion.Server.retries + 1}th times.`);
+                        reject(err);
+                    }
+                }
+                else {
+                    clearInterval(intervalID);
+                    Log.info(logTags, `Server listening on port ${port}.`);
+                    resolve();
+                }
+            });
+        }, Config.connexion.Server.timeout);
     });
 }
 
 function initMySQL() {
+    let mysqlConnectRetries = Config.connexion.MySQL.retries;
+    let mysqlConnectIntervalID = null;
+
     return new Promise((resolve, reject) => {
-        mysqlConnection.connect((err) => {
-            if (err) {
-                Log.err(logTags, `MySQL connection (${mysqlConnection.config.host}:${mysqlConnection.config.port}) failed.`);
-                reject(err);
-            }
-            else {
-                Log.info(logTags, `MySQL connection (${mysqlConnection.config.host}:${mysqlConnection.config.port}) established.`);
-                resolve();
-            }
-        });
+        mysqlConnectIntervalID = setInterval(() => {
+            mysqlConnection.connect((err) => {
+                if (err) {
+                    Log.err(logTags, `MySQL connection (${mysqlConnection.config.host}:${mysqlConnection.config.port}) failed.`);
+                    if (mysqlConnectRetries > 0) {
+                        mysqlConnectRetries--;
+                    }
+                    else {
+                        clearInterval(mysqlConnectIntervalID);
+                        Log.err(logTags, `MySQL connection failed for the ${Config.connexion.MySQL.retries + 1}th times.`);
+                        reject(err);
+                    }
+                }
+                else {
+                    clearInterval(mysqlConnectIntervalID);
+                    Log.info(logTags, `MySQL connection (${mysqlConnection.config.host}:${mysqlConnection.config.port}) established.`);
+                    resolve();
+                }
+            });
+        }, Config.connexion.MySQL.timeout);
     });
 }
 
@@ -140,6 +171,9 @@ function initRoutes() {
             res.json({ status: "ok", message: "You're now disconnected." });
         }
     });
+    app.get("/news", (req, res) => {
+        res.promise(routes.Common.getNews(req, res, mysqlConnection));
+    });
     // USER FEEDS
     app.get("/user/feeds", (req, res) => {
         if (!req.cookies.sagasphere_user) {
@@ -158,17 +192,17 @@ function initRoutes() {
             res.promise(routes.User.getNews(req, res, mysqlConnection));
         }
     });
-    // COMMON NEWS
-    app.get("/news", (req, res) => {
+    // COMMON SAGA NEWS
+    app.get("/saga/news", (req, res) => {
         if (!req.cookies.sagasphere_user) {
             res.status(401).json({ status: "ko", message: "You're not connected." });
         }
         else {
-            res.promise(routes.Common.getNews(req, res, mysqlConnection));
+            res.promise(routes.Common.getSagaNews(req, res, mysqlConnection));
         }
     });
     // COMMON SAGA LIST
-    app.get("/sagalist", (req, res) => {
+    app.get("/saga/list", (req, res) => {
         if (!req.cookies.sagasphere_user) {
             res.status(401).json({ status: "ko", message: "You're not connected." });
         }
--- sagasphere/src/utils/config.js
+++ sagasphere/src/utils/config.js
@@ -1,4 +1,19 @@
+const global = {
+    retries: process.env.SAGASPHERE_GLOBAL_RETRIES || 3,
+    timeout: process.env.SAGASPHERE_GLOBAL_TIMEOUT || 10000
+};
+
 export const Config = {
+    connexion: {
+        Server: {
+            retries: global.retries,
+            timeout: global.timeout
+        },
+        MySQL: {
+            retries: global.retries,
+            timeout: global.timeout
+        }
+    },
     requests: {
         timeout: 10000
     }
